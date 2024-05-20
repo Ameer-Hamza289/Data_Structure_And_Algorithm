8e8c056831ccc87622e85633f279e92850b777c7
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-05-20 13:04:59 +0500

    Solved by first sorting and then comparing i by i-1

--- contain_duplicate.cpp
+++ contain_duplicate.cpp
@@ -1,28 +1,34 @@
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 class Solution {
 public:
+    // bool containsDuplicate(vector<int>& nums) {
+    //     bool duplicate=false;
+    //     int i=0;
+    //     for(auto number:nums){
+    //         if(duplicate) break;
+    //         int index=0;
+    //         int count=0;
+    //         while(index<nums.size() && !duplicate){
+    //             if(number==nums[index]) {
+    //                 count++;
+    //             }
+    //             if(count>1){
+    //                 duplicate=true;
+    //                 break;
+    //             }
+    //             index++;
+    //         }
+    //         i++;
+    //         count=0;
+    //     }
+    //     return duplicate;
+    // }
     bool containsDuplicate(vector<int>& nums) {
-        bool duplicate=false;
-        int i=0;
-        for(auto number:nums){
-            if(duplicate) break;
-            int index=0;
-            int count=0;
-            while(index<nums.size() && !duplicate){
-                if(number==nums[index]) {
-                    count++;
-                }
-                if(count>1){
-                    duplicate=true;
-                    break;
-                }
-                index++;
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==nums[i-1]) return true;
             }
-            i++;
-            count=0;
-        }
-        return duplicate;
+        return false;
     }
 };
 
-// this  solution causing time limit exceed problem
