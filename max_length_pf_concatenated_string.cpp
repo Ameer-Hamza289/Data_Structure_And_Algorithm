ae9c52e2d14c9f0b022b5a63247050f703bc9a09
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-07-02 23:26:02 +0500

    Max length concatenated string

--- max_length_pf_concatenated_string.cpp
+++ max_length_pf_concatenated_string.cpp
@@ -1,44 +1,44 @@
-//Maximum Length of a Concatenated String with Unique Characters
-
 class Solution {
-public:
-
+private:
+    int ans=0;
     bool uniqueChar(string& str){
+        // sort(str.begin(),str.end());
+        // for(int i=1;i<str.size();i++){
+        //     if(str[i]==str[i-1]){
+        //         return false;
+        //     }
+        // }
         int store=0;
         for(int i=0;i<str.size();i++){
-            int idxBit= str[i]-'a';
-            if((store & (1<<idxBit))>0){
+            int idxBit=str[i]-'a';
+            if((store && (1<<idxBit))>0){
                 return false;
             }
-            store= store | (1<<idxBit);
+            store=store | (1<<idxBit);
         }
         return true;
     }
-    int ans=0;
-    void helper(vector<string>& arr, int idx,vector<string>& temp){
-       //  ["ui", "qe"]
+public:
+    void helper(vector<string>&arr,int idx, vector<string>& temp){
         string str="";
         for(int i=0;i<temp.size();i++){
-            str+= temp[i];
+            str+=temp[i];
         }
         if(uniqueChar(str)){
             int l=str.size();
-            ans=max(ans, l);
+            ans=max(ans,l);
         }
-
-
-        for(int i=idx;i<arr.size();i++){
+        for(int i=idx; i< arr.size();i++){
             temp.push_back(arr[i]);
-            if(uniqueChar(arr[i]))
-                helper(arr,i+1,temp);
+            if(uniqueChar(arr[i])){
+                helper(arr,i+1,temp);   
+            }
             temp.pop_back();
         }
-
     }
     int maxLength(vector<string>& arr) {
         vector<string> temp;
-        helper(arr, 0, temp);
+        helper(arr,0,temp);
         return ans;
-        
     }
 };
