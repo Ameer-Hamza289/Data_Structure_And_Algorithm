df782115d225f05a9e6c4fcc47a3ea066c44d9a1
Ameer Hamza <121759912+Ameer-Hamza289@users.noreply.github.com>
2025-07-15 12:03:13 +0500

    Time: 51 ms (57.47%) | Memory: 91 MB (31.35%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> duplicate_checker = {};
-        bool hasDuplicate = false;
-        for (int i = 0; i < nums.size(); i++) {
-            if (duplicate_checker.find(nums[i]) != duplicate_checker.end()) {
-                hasDuplicate = true;
-                break;
+        unordered_set<int> duplicate={};
+        for(int i=0; i<nums.size();i++){
+            if(duplicate.find(nums[i])!=duplicate.end()){
+                return true;
+            }else{
+                duplicate.insert(nums[i]);
             }
-            duplicate_checker.insert(nums[i]);
         }
-
-        // for(int i = 0; i < nums.size(); i++){
-        //     for (int j=i+1; j< nums.size();j++){
-        //         if(nums[i]==nums[j]){
-        //             hasDuplicate = true;
-        //             break;
-        //         }
-        //     }
-        //     if(hasDuplicate){
-        //         break;
-        //     }
-        // }
-        return hasDuplicate;
+        return false;
     }
 };
